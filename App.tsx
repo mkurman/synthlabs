5f17c597716a37e791fe302da1004195b1341907
Mariusz Kurman <mariusz.kurman@gmail.com>
2026-02-01 04:43:18 +0100

    refactor: remove unused Ollama state variables for cleaner code

--- App.tsx
+++ App.tsx
@@ -83,13 +83,6 @@ export default function App() {
         refreshOllamaModels
     } = useOllama({ externalProvider, externalModel, setExternalModel });
 
-    const {
-        ollamaModels,
-        ollamaStatus,
-        ollamaLoading,
-        refreshOllamaModels
-    } = useOllama({ externalProvider, externalModel, setExternalModel });
-
     // --- State: Generation Params ---
     const [generationParams, setGenerationParams] = useState<GenerationParams>({});
 
